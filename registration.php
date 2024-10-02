c6ae07015f22a6cf510db26656f3d336d40dcedd
plumrocket <devteam@plumrocket.com>
2024-10-02 12:27:51 +0300

    Add rewrite for dom-observer.js

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package     Plumrocket_ModuleUiFixMagento247
+ * @copyright   Copyright (c) 2024 Plumrocket Inc. (https://plumrocket.com)
+ * @license     https://plumrocket.com/license   End-user License Agreement
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Plumrocket_ModuleUiFixMagento247', __DIR__);
---
+++ view/base/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * @package     Plumrocket_ModuleUiFixMagento247
+ * @copyright   Copyright (c) 2024 Plumrocket Inc. (https://plumrocket.com)
+ * @license     https://plumrocket.com/license   End-user License Agreement
+ */
+
+var config = {
+    map: {
+        '*': {
+            'Magento_Ui/js/lib/view/utils/dom-observer': 'Plumrocket_ModuleUiFixMagento247/js/lib/view/utils/dom-observer'
+        }
+    }
+};
